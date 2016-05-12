9e7caed67b1632a152f406485bbab204bf671355
Holger Schauf <holger.schauf@googlemail.com>
2016-05-12 09:55:11 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+class ListScrollBound extends Component {
+
+    constructor (props) {
+        super(props);
+    }
+
+    preventBodyScrolling(e) {
+        const el = e.currentTarget;
+
+        if (el.clientHeight + el.scrollTop + e.deltaY >= el.scrollHeight) {
+            e.preventDefault();
+            el.scrollTop = el.scrollHeight;
+        } else if (el.scrollTop + e.deltaY <= 0) {
+            e.preventDefault();
+            el.scrollTop = 0;
+        }
+    }
+
+    render() {
+
+        return (
+            <this.props.tagName onWheel={this.preventBodyScrolling} {...this.props}>
+                {this.props.children}
+            </this.props.tagName>
+        );
+    }
+}
+
+ListScrollBound.propTypes = {
+    tagName: React.PropTypes.string,
+    className: React.PropTypes.string
+}
+
+ListScrollBound.defaultProps = {
+    tagName: 'ul',
+    className: null
+}
+
+module.exports = ListScrollBound;
